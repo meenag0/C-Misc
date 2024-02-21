60ea8cc08bbb998640c348388ae4d04a11707dcf
meenag0 <139080002+meenag0@users.noreply.github.com>
2024-02-20 22:11:20 -0500

    Started Problem 2: Temp Conversions

---
+++ TempConversion.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float temp;
+    cout << "Please enter your temperature: ";
+    cin >> temp;
+    if(){}
+    char unit;
+    cout << "Please enter the units (F/C): ";
+    cin >> unit;
+    char f = 'f';
+    char F = 'F';
+    char C = 'C';
+    char c = 'c';
+    if (unit == f || unit == F){
+
+    } else if (unit == c || unit == C){
+        
+    } else{
+
+    }
+
+}
