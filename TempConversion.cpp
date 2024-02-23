46d526bcb4f03b859f2db89d57ca095a32d2bf01
meenag0 <139080002+meenag0@users.noreply.github.com>
2024-02-23 12:50:15 -0500

    Added logic and base code for tempconvert, need to fix some grammar

--- TempConversion.cpp
+++ TempConversion.cpp
@@ -1,24 +1,65 @@
 #include <iostream>
 using namespace std;
+#include <ctype.h>
+
+void YesorNo(char YorN);
+void convertTemp(char unit, float temp);
+
+void YesorNo(char YorN){
+        if (YorN == 'Y' || YorN == 'y'){
+            convertTemp(' ', 0);        
+        } else if (YorN == 'N' || YorN == 'n'){
+            cout << "Thank you for playing.";
+        } else{
+            cout << "Please enter a valid input.";
+            cout << "Do you want another conversion?";
+            cin >> YorN;
+            YesorNo(YorN);
+        }
+}
+
+void convertTemp(char unit, float temp){
+        cout << "Please enter your temperature: ";
+        cin >> temp;
+
+        cout << "Please enter the units (F/C): ";
+        cin >> unit;
+
+        float resultTemp;
+        if (unit == 'F' || unit == 'f') {            
+            resultTemp = (temp - 32 ) * 5 / 9;
+        } else if (unit == 'C' || unit == 'c') {
+            resultTemp = (temp  * 9 / 5) + 32;
+        } else{
+            cout << "Please enter a valid input.";
+            convertTemp(' ', 0);
+            return;
+        }
+    
+        char YorN;
+        cout << "A temperature of" << temp <<  "degrees Celsius is equivalent to" << resultTemp << " degrees Fahrenheit.";
+        cout << "Do you want another conversion?";
+        cin >> YorN;
+
+        if (YorN == 'Y' || YorN == 'y'){
+            convertTemp(' ', 0);
+        } else if (YorN == 'N' || YorN == 'n'){
+            cout << "Thank you for playing.";
+        } else{
+            cout << "Please enter a valid input.";
+            cout << "Do you want another conversion?";
+            cin >> YorN;
+            YesorNo(YorN);
+        }
+}
+
+
 
 int main(){
-    float temp;
-    cout << "Please enter your temperature: ";
-    cin >> temp;
-    if(){}
-    char unit;
-    cout << "Please enter the units (F/C): ";
-    cin >> unit;
-    char f = 'f';
-    char F = 'F';
-    char C = 'C';
-    char c = 'c';
-    if (unit == f || unit == F){
 
-    } else if (unit == c || unit == C){
-        
-    } else{
+    convertTemp(' ', 0);
 
-    }
+
+    return 0;
 
 }
