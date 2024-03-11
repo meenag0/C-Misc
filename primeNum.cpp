a2eebfd7e8d546f671d976febe63dd24129d8895
meenag0 <139080002+meenag0@users.noreply.github.com>
2024-03-11 01:44:05 -0400

    prime number printing

---
+++ primeNum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  for (int i{1}; i < 10000; i++) {
+    bool checkPrime = true;
+    for (int j{2}; j < i; j++) {
+      if (i % j == 0) {
+        checkPrime = false;
+        break;
+      }
+    }
+    if (checkPrime == true) {
+      cout << i << endl;
+    }
+  }
+}
+
